346574c8babfcbee653dd1f166f947508ed787ad
cabbage9 <iherewithmyheart@gmail.com>
2021-06-03 14:03:18 +0800

    🔧 config docs

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,13 +1,10 @@
-const { getSidebar } = require("../../scripts/utils");
-
-/**
- * @type {import('vitepress').UserConfig}
- */
 module.exports = {
   head: [
-    // ["link", { rel: "icon", href: "/logo.png", type: "image/png" }],
+    [
+      "link",
+      { rel: "icon", href: "/book-mark-fill-24px.png", type: "image/png" },
+    ],
     ["meta", { name: "author", content: "cabbage9" }],
-    // 设置 描述 和 关键词
     ["meta", { name: "keywords", content: "vitepress 中文 文档" }],
     [
       "meta",
@@ -25,14 +22,74 @@ module.exports = {
     repo: "cabbage9/cn-vitepress",
     docsBranch: "master",
     docsDir: "docs",
-
+    algolia: {
+      apiKey: "b564625be65feb637a8f776517d5b143",
+      indexName: "cn-vitepress_NAME",
+    },
     editLinks: true,
     editLinkText: "在帮助我们在GitHub上改善此页",
     lastUpdated: "上次更新",
 
-    nav: [{ text: "配置参考", link: "/config/app/basics" }],
+    nav: [
+      { text: "指南", link: "/guide/introduction/what-is-vitepress/" },
+      { text: "配置参考", link: "/config/app/basics" },
+    ],
 
     sidebar: {
+      "/guide/": [
+        {
+          text: "介绍",
+          children: [
+            {
+              text: "什么是VitePress",
+              link: "/guide/introduction/what-is-vitepress/",
+            },
+            {
+              text: "入门",
+              link: "/guide/introduction/getting-started/",
+            },
+            {
+              text: "配置",
+              link: "/guide/introduction/configuration/",
+            },
+            {
+              text: "静态资源处理",
+              link: "/guide/introduction/asset-handling/",
+            },
+            {
+              text: "Markdown 扩展",
+              link: "/guide/introduction/markdown-extensions/",
+            },
+            {
+              text: "在 Markdown 中使用 Vue",
+              link: "/guide/introduction/using-vue-in-markdown/",
+            },
+            {
+              text: "部署",
+              link: "/guide/introduction/depolying/",
+            },
+          ],
+        },
+        {
+          text: "进阶",
+          children: [
+            { text: "Frontmatter", link: "/guide/advanced/frontmatter/" },
+            {
+              text: "全局计算",
+              link: "/guide/advanced/global-computed/",
+            },
+            {
+              text: "全局组件",
+              link: "/guide/advanced/global-component/",
+            },
+            { text: "自定义", link: "/guide/advanced/customization/" },
+            {
+              text: "与 VuePress 的区别",
+              link: "/guide/advanced/differences-from-vuepress/",
+            },
+          ],
+        },
+      ],
       "/config/": [
         {
           text: "应用配置",
