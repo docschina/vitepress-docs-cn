11eaaf056224f60c5a334a3ad3ae82ad9ebc9030
cabbage9 <iherewithmyheart@gmail.com>
2021-06-09 16:50:34 +0800

    🎨 use ' & 🌐 english translation link

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,98 +1,105 @@
 module.exports = {
-  base: "/",
-  lang: "zh-CN",
-  title: "cn-vitepress",
-  description: "Vite & Vue powered static site generator - vitepress cn docs.",
+  base: '/',
+  lang: 'zh-CN',
+  title: 'VitePress',
+  description: 'Vite & Vue powered static site generator - vitepress cn docs.',
   themeConfig: {
-    repo: "cabbage9/cn-vitepress",
-    docsBranch: "master",
-    docsDir: "docs",
-    algolia: {
-      apiKey: "b564625be65feb637a8f776517d5b143",
-      indexName: "cn-vitepress_NAME",
-    },
+    repo: 'cabbage9/cn-vitepress',
+    docsBranch: 'master',
+    docsDir: 'docs',
+    // algolia: {
+    //   apiKey: 'b564625be65feb637a8f776517d5b143',
+    //   indexName: 'cn-vitepress_NAME',
+    // },
     editLinks: true,
-    editLinkText: "在帮助我们在GitHub上改善此页",
-    lastUpdated: "上次更新",
+    editLinkText: '在帮助我们在GitHub上改善此页',
+    lastUpdated: '上次更新',
 
     nav: [
-      { text: "指南", link: "/guide/introduction/what-is-vitepress/" },
-      { text: "配置参考", link: "/config/app/basics/" },
+      { text: '指南', link: '/guide/introduction/what-is-vitepress/' },
+      { text: '配置参考', link: '/config/app/basics/' },
       {
-        text: "发行说明",
-        link: "https://github.com/vuejs/vitepress/releases",
+        text: '发行说明',
+        link: 'https://github.com/vuejs/vitepress/releases',
+      },
+      {
+        text: '多语言',
+        items: [
+          { text: '简体中文', link: 'https://cn-vitepress.netlify.app/' },
+          { text: 'English', link: 'https://vitepress.vuejs.org/' },
+        ],
       },
     ],
 
     sidebar: {
-      "/guide/": [
+      '/guide/': [
         {
-          text: "介绍",
+          text: '介绍',
           children: [
             {
-              text: "什么是VitePress?",
-              link: "/guide/introduction/what-is-vitepress/",
+              text: '什么是VitePress?',
+              link: '/guide/introduction/what-is-vitepress/',
             },
             {
-              text: "入门",
-              link: "/guide/introduction/getting-started/",
+              text: '入门',
+              link: '/guide/introduction/getting-started/',
             },
             {
-              text: "配置",
-              link: "/guide/introduction/configuration/",
+              text: '配置',
+              link: '/guide/introduction/configuration/',
             },
             {
-              text: "静态资源处理",
-              link: "/guide/introduction/asset-handling/",
+              text: '静态资源处理',
+              link: '/guide/introduction/asset-handling/',
             },
             {
-              text: "Markdown 扩展",
-              link: "/guide/introduction/markdown-extensions/",
+              text: 'Markdown 扩展',
+              link: '/guide/introduction/markdown-extensions/',
             },
             {
-              text: "在 Markdown 中使用 Vue",
-              link: "/guide/introduction/using-vue-in-markdown/",
+              text: '在 Markdown 中使用 Vue',
+              link: '/guide/introduction/using-vue-in-markdown/',
             },
             {
-              text: "部署",
-              link: "/guide/introduction/depolying/",
+              text: '部署',
+              link: '/guide/introduction/depolying/',
             },
           ],
         },
         {
-          text: "进阶",
+          text: '进阶',
           children: [
-            { text: "Frontmatter", link: "/guide/advanced/frontmatter/" },
+            { text: 'Frontmatter', link: '/guide/advanced/frontmatter/' },
             {
-              text: "全局计算属性",
-              link: "/guide/advanced/global-computed/",
+              text: '全局计算属性',
+              link: '/guide/advanced/global-computed/',
             },
             {
-              text: "全局组件",
-              link: "/guide/advanced/global-component/",
+              text: '全局组件',
+              link: '/guide/advanced/global-component/',
             },
-            { text: "自定义", link: "/guide/advanced/customization/" },
+            { text: '自定义', link: '/guide/advanced/customization/' },
             {
-              text: "与 VuePress 的区别",
-              link: "/guide/advanced/differences-from-vuepress/",
+              text: '与 VuePress 的区别',
+              link: '/guide/advanced/differences-from-vuepress/',
             },
           ],
         },
       ],
-      "/config/": [
+      '/config/': [
         {
-          text: "应用配置",
-          children: [{ text: "基础", link: "/config/app/basics/" }],
+          text: '应用配置',
+          children: [{ text: '基础', link: '/config/app/basics/' }],
         },
         {
-          text: "主题配置",
+          text: '主题配置',
           children: [
-            { text: "主页", link: "/config/theme/homepage/" },
-            { text: "Algolia 搜索", link: "/config/theme/algolia-search/" },
-            { text: "Carbon 广告", link: "/config/theme/carbon-ads/" },
+            { text: '主页', link: '/config/theme/homepage/' },
+            { text: 'Algolia 搜索', link: '/config/theme/algolia-search/' },
+            { text: 'Carbon 广告', link: '/config/theme/carbon-ads/' },
           ],
         },
       ],
     },
   },
-};
+}
